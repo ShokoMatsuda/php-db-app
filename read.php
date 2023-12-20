4b4fa5b9d1753c3e16896ebf1651a4b4c5b34239
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-20 21:12:15 +0900

    12.4 並び替え機能と検索機能を同時に使えるようにしよう

--- read.php
+++ read.php
@@ -72,13 +72,14 @@ try {
             <h1>商品一覧</h1>
             <div class="products-ui">
                 <div>
-                    <a href="read.php?order=desc">
+                    <a href="read.php?order=desc&keyword=<?= $keyword ?>">
                         <img src="images/desc.png" alt="降順に並び替え" class="sort-img">
                     </a>
-                    <a href="read.php?order=asc">
+                    <a href="read.php?order=asc&keyword=<?= $keyword ?>">
                         <img src="images/asc.png" alt="昇順に並び替え" class="sort-img">
                     </a>
                     <form action="read.php" method="get" class="search-form">
+                        <input type="hidden" name="order" value="<?= $order ?>">
                         <input type="text" class="search-box" placeholder="商品名で検索" name="keyword" value="<?= $keyword ?>">
                     </form>
                 </div>
