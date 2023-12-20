518b14c4fd0d45875bc42b643adc5f4c79b56e59
ShokoMatsuda <syokom.0128@gmail.com>
2023-12-20 23:01:44 +0900

    14.2 商品一覧ページに編集ボタンを追加しよう

--- read.php
+++ read.php
@@ -98,6 +98,7 @@ try {
                         <th>単価</th>
                         <th>在庫数</th>
                         <th>仕入先コード</th>
+                        <th>編集</th>
                     </tr>
                     <?php
                     // 配列の中身を順番に取り出し、表形式で出力する
@@ -109,6 +110,7 @@ try {
                         <td>{$product['price']}</td>
                         <td>{$product['stock_quantity']}</td>
                         <td>{$product['vendor_code']}</td>
+                        <td><a href='update.php?id={$product['id']}'><img src='images/edit.png' alt='編集' class='edit-icon'></td>
                         </tr>
                         ";
                         echo $table_row;
